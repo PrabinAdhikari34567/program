1af9778f5c0f9be6892f9fa19f9cc1884f6796bb
PrabinAdhikari34567 <prabin.adhikari34567@gmail.com>
2016-11-16 23:40:53 -0800

    Add files via upload

---
+++ Print your name.c
@@ -0,0 +1,20 @@
+/*
+Student Name:Prabin Adhikari
+Subject:Programming Fundamental
+Roll No:
+Program:Computer Science in information technology
+Lab sheet:2
+Date:16 nov, 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char s [15];
+    printf("What is your name? \n");
+    scanf("%s",&s);
+    printf("Name of user is %s" , s);
+    getch();
+    return(0);
+}
