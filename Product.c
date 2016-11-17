2bdd2b3b39ee8733e78626d4113b26e95abf2b07
PrabinAdhikari34567 <prabin.adhikari34567@gmail.com>
2016-11-16 23:41:35 -0800

    Add files via upload

---
+++ Product.c
@@ -0,0 +1,27 @@
+/*
+Student Name:Prabin Adhikari
+Subject:Programming Fundamental
+Roll No:
+Program:Computer Science in information technology
+Lab sheet:2
+Date:16 nov, 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+    float a,b, product;
+    printf("Enter Your first number\n");
+    scanf("%f" , &a);
+    printf("Enter Second number\n");
+    scanf("%f", &b);
+    product=a*b;
+    printf("The Product Of two number is %f", product);
+    getch();
+    return(0);
+
+
+}
+
