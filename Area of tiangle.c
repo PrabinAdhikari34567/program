04714048e1aa3b7ee8a956ad38b8be39aa4cabcd
PrabinAdhikari34567 <prabin.adhikari34567@gmail.com>
2016-11-16 23:40:13 -0800

    Add files via upload

---
+++ Area of tiangle.c
@@ -0,0 +1,31 @@
+
+/*
+Student Name:Prabin Adhikari
+Subject:Programming Fundamental
+Roll No:
+Program:Computer Science in information technology
+Lab sheet:2
+Date:16 nov, 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+
+
+
+
+    float Base,Height,area ;
+    printf("Enter Base\n");
+    scanf("%f",&Base);
+    printf("Enter Height\n");
+    scanf("%f",&Height);
+   area=0.5*Base*Height;
+
+    printf("Area of triangle is %f",area);
+    getch();
+    return(0);
+
+}
