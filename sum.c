3e631d1838a27d4ca29f65e257da3bea2ea54652
PrabinAdhikari34567 <prabin.adhikari34567@gmail.com>
2016-11-16 23:42:01 -0800

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,23 @@
+/*
+Student Name:Prabin Adhikari
+Subject:Programming Fundamental
+Roll No:
+Program:Computer Science in information technology
+Lab sheet:2
+Date:16 nov, 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int a,b, sum;
+    printf("Enter first number\n");
+    scanf("%d", &a);
+    printf("Enter second number\n");
+    scanf("%d", &b);
+    sum=a+b;
+    printf( "sum of two numbes are %d" ,sum);
+    getch();
+    return(0);
+}
