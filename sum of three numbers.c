843cd7d0477ce8110f8de28ef7fce4307f326e2a
PrabinAdhikari34567 <prabin.adhikari34567@gmail.com>
2016-11-23 10:15:01 +0545

    Add files via upload

---
+++ sum of three numbers.c
@@ -0,0 +1,25 @@
+/*
+Student Name:Prabin Adhikari
+Subject:Programming Fundamental
+Roll No:
+Program:Computer Science in information technology
+Lab sheet:2
+Date:16 nov, 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c, sum;
+    printf("Enter first number\n");
+    scanf("%d", &a);
+    printf("Enter second number\n");
+    scanf("%d", &b);
+    printf("Enter third number\n");
+    scanf("%d", &c);
+
+    sum=a+b+c;
+    printf( "sum of three numbes are %d" ,sum);
+    getch();
+    return(0);
+}
