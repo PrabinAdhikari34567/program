dfe4168b8de17b2ee6936ae9ac13db9b8e7041e4
PrabinAdhikari34567 <prabin.adhikari34567@gmail.com>
2016-11-23 10:15:33 +0545

    Add files via upload

---
+++ temperature.c
@@ -0,0 +1,23 @@
+/*
+Student Name:Prabin Adhikari
+Subject:Programming Fundamental
+Roll No:
+Program:Computer Science in information technology
+Lab sheet:2
+Date:16 nov, 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    float f, c;
+    printf("Please Enter fahrenheit\n");
+    scanf("%f" , &f);
+    c-0.55*(f-32);
+
+    printf("the configrade is :%f\n", c);
+    getch();
+    return(0);
+
+
+}
