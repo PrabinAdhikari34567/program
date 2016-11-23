ad6b58e242ba5e95b166765bb30a9e9055adafad
PrabinAdhikari34567 <prabin.adhikari34567@gmail.com>
2016-11-23 12:35:25 +0545

    Add files via upload

---
+++ ASCIT.C
@@ -0,0 +1,25 @@
+
+/*
+Student Name:Prabin Adhikari
+Subject:Programming Fundamental
+Roll No:
+Program:Computer Science in information technology
+Lab sheet:2
+Date:16 nov, 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+ int main()
+ {
+    char c;
+
+    int a;
+    printf("Enter a character: ");
+
+    scanf("%c", &c);
+    a=c;
+
+    printf("ASCII value of %d",a);
+    return 0;
+}
+
