93641dbd4333cd263ece58a07e4383ed81f0aff9
PrabinAdhikari34567 <prabin.adhikari34567@gmail.com>
2016-11-24 11:11:10 +0545

    Add files via upload

---
+++ if program positive .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ int main()
+ {
+     int n;
+     printf("Enter Number\n");
+     scanf("%d", &n);
+
+     if (n>0)
+     {
+         printf("Number is Positive %d",n);
+     }
+
+     return(0);
+ }
+
