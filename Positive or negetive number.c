ae94b20aef40db5dda504e331343233a3a776577
PrabinAdhikari34567 <prabin.adhikari34567@gmail.com>
2016-11-27 18:34:44 +0545

    Add files via upload

---
+++ Positive or negetive number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int n;
+    printf("Enter the number\n");
+    scanf("%d",&n);
+
+    if(n>0)
+    {
+        printf("The number is positive");
+
+    }
+    else
+    {
+        printf("The number is negative");
+    }
+    getch();
+    return(0);
+
+
+}
