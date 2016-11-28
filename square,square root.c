f8f0c97647a38f0a1875af05cf59ac974c3582c0
PrabinAdhikari34567 <prabin.adhikari34567@gmail.com>
2016-11-28 12:38:47 +0545

    Update square,square root.c

--- square,square root.c
+++ square,square root.c
@@ -1,4 +1,11 @@
-
+/*
+Student Name:Prabin Adhikari
+Subject:Programming Fundamental
+Roll No:
+Program:Computer Science in information technology
+Lab sheet:2
+Date:16 nov, 2016
+*/
 
 #include<stdio.h>
 #include<conio.h>
