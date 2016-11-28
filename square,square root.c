ac65fa5cbf145b38f615b5bcbd85c6ef9519c834
PrabinAdhikari34567 <prabin.adhikari34567@gmail.com>
2016-11-28 12:37:42 +0545

    Add files via upload

---
+++ square,square root.c
@@ -0,0 +1,28 @@
+
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+    float a, square,sr;
+    printf("Enter Number\n");
+    scanf("%f",&a);
+
+    square=pow(a,2);
+    sr=pow(a,0.5);
+    sr=pow(a,0.5);
+
+    printf("Square of;%f\n", square);
+    printf("Square root is %f", sr);
+
+
+
+
+    getch();
+    return(0);
+
+
+
+
+}
