d97ef7c8cba3ff8f6c7283effd6c9de0e86d1822
PrabinAdhikari34567 <prabin.adhikari34567@gmail.com>
2016-11-30 12:42:04 +0545

    Add files via upload

---
+++ operatorr.c
@@ -0,0 +1,39 @@
+/*
+Student Name:Prabin Adhikari
+Subject:Programming Fundamental
+Roll No:
+Program:programming fundamental
+Lab sheet:6
+Date:30 nov, 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+
+int a,b ;
+
+printf("Enter first Number\n");
+scanf("%d",&a);
+printf("Enter second Number\n");
+scanf("%d",&b);
+
+a+=b;
+printf("+=%d\n",a);
+
+a-=b;
+printf("-=%d\n",a);
+a*=b;
+printf("*=%d\n",a);
+a/=b;
+printf("/=%d\n",a);
+
+
+
+
+
+
+getch();
+return(0);
+}
