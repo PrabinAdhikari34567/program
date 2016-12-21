336d00384ddc3df609788c03c09fb695fb879c23
PrabinAdhikari34567 <prabin.adhikari34567@gmail.com>
2016-12-21 10:21:46 +0545

    Add files via upload

---
+++ prabin.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+
+int main()
+{
+	char c;
+	printf("Enter a Character\n");
+	scanf("%c",&c);
+	
+	if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I'|| c=='O'|| c=='U' )
+	{
+		printf("The number is vowel %c",c);
+		
+	}
+	else{
+		printf("%c  is not a vowel",c);
+	}
+		
+	return 0;
+	
+}
+
